1e1be8318727b1979ae61fa03340dc69a3e8f637
omathot <omathot_dev@proton.me>
2024-10-04 15:10:13 +0200

    added macro content

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -22,9 +22,61 @@ mod tests {
 
     #[test]
     fn make_variating_name_struct() {
-        let test = make![Foo, "hello"];
+        let test = make![Foo, "hello",];
 
         assert_eq!(Foo::build("hello"), test);
+    }
+
+    #[test]
+    fn make_vec_of_strings() {
+        let v = make_vec_strings!["hey", "bye", "help"];
+
+        assert_eq!(vec!["hey", "bye", "help"], v);
+    }
+
+    #[test]
+    fn swap_macro() {
+        let mut s1 = String::from("Hey");
+        let mut s2 = String::from("Bye");
+
+        swap![s1, s2]; //warning for manual swap, nothing wrong
+    }
+
+    #[test]
+    fn add_macro() {
+        let x = add![1, 1];
+        let y = add![1, 1, 1, 1, 1];
+
+        assert_eq!(2, x);
+        assert_eq!(5, y);
+    }
+
+    #[test]
+    fn make_vec_ints_macro() {
+        let v = make_vec_ints![1, 2, 3];
+
+        assert_eq!(vec![1, 2, 3], v);
+    }
+
+    make_fancy_struct!(Test);
+    make_fancy_struct!(TestWithNames, name, ability, weakness);
+    #[test]
+    fn make_fancy_struct_macro() {
+        let s = Test { power: 1 };
+        let s2 = Test::new(2);
+
+        let ss = TestWithNames {
+            power: 1,
+            name: String::from("John"),
+            ability: String::from("speed"),
+            weakness: String::from("cold"),
+        };
+        let ss2 = TestWithNames::new(1);
+
+        assert_eq!(Test { power: 1 }, s);
+        assert_eq!(Test { power: 2 }, s2);
+        assert_eq!("John".to_string(), ss.name);
+        assert_eq!("", ss2.name);
     }
 
     #[test]
--- macros/src/make.rs
+++ macros/src/make.rs
@@ -27,7 +27,67 @@ macro_rules! make {
         <$type>::new()
     };
 
+    // $(,)? only serves to allow trailing , after last item
     ($type:ty, $($arg:expr),+ $(,)?) => {
         <$type>::build($($arg),+)
+    };
+}
+
+// $($arg:expr),+ has to happen at least once
+// $($arg:expr),* can happen zero or many times
+
+macro_rules! make_vec_strings {
+    ($($arg:expr),*) => {
+        vec![$($arg.to_string()),*]
+    };
+}
+
+macro_rules! add {
+    ($x:expr, $y:expr) => {
+        $x + $y
+    };
+    ($x:expr, $($y:expr),+) => {
+        $x + add![$($y),+]
+    }
+}
+
+macro_rules! swap {
+    ($x:expr, $y:expr) => {
+        let temp = $x;
+        $x = $y;
+        $y = temp;
+    };
+}
+
+macro_rules! enum_with_method {
+    ($(#[$meta:meta])* $vis:vis enum $name:ident {
+        $($(#[$vmeta:meta])* $vname:ident $(= $val:expr)?),*$(,)?
+    }) => {
+        //
+    };
+}
+
+macro_rules! make_vec_ints {
+    ($($x:expr),*) => {
+        vec![$($x), *]
+    };
+}
+
+// make a struct with implemented methods
+macro_rules! make_fancy_struct {
+    ($name:ident$(, $($ef:ident),*)?) => {
+        #[derive(Debug, Eq, PartialEq, Clone)]
+        pub struct $name {
+            power: usize,
+            $($($ef: String,)*)?
+        }
+        impl $name {
+            pub fn new(n: usize) -> Self {
+                $name {
+                    power: n,
+                    $($($ef: String::new(),)*)?
+                }
+            }
+        }
     };
 }
